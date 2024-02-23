6f44af9a44df2546bb87bccf832e1a988055b69e
Aaron-Trippy <aarontripp83@gmail.com>
2024-02-23 11:15:35 -0800

    update navbar desktop

--- src/pages/components/NavBarDesktop/index.tsx
+++ src/pages/components/NavBarDesktop/index.tsx
@@ -4,18 +4,20 @@ import styles from './NavBarDesktop.module.css';
 
 const DesktopNavbar = () => {
     return (
-        <nav className={styles.desktopNavbar}>
-            <div className={styles.navLinks}>
-                <div className={styles.title}>
-                    <img src="/icons/Logo Green.png" alt="Logo" />
-                    <Link href="/">Culinary Compass</Link>
-                </div>
-                <Link href="/">Home</Link>
-                <Link href="/recipes">Recipe</Link>
-                <Link href="/restaurants">Restaurants</Link>
-                <Link href="/info">Info</Link>
+        <div className={styles.desktopNavbar}>
+            <div className={styles.content}>
+            <div className={styles.logo}>
+                <img src="/icons/Logo Green.png" alt="Logo" />
+                <Link href="/">Culinary Compass</Link>
             </div>
-        </nav>
+            <div className={styles.navLinks}>
+            <Link href="/">Home</Link>
+            <Link href="/recipes">Recipe</Link>
+            <Link href="/restaurants">Restaurants</Link>
+            <Link href="/info">Info</Link>
+        </div>
+        </div>
+    </div>
     );
 };
 
