acda15410690bfe067e06a6b793325b79b26bb4a
EricQian01 <ericqian1203@gmail.com>
2024-03-27 12:54:29 -0700

    css naming update

--- src/components/NavBarDesktop/index.tsx
+++ src/components/NavBarDesktop/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from './NavbarDesktop.module.css'; 
+import styles from './NavBarDesktop.module.css'; 
 
 const NavbarDesktop = () => {
     return (
--- src/components/NavBarMobile/index.tsx
+++ src/components/NavBarMobile/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from './NavbarMobile.module.css';
+import styles from './NavBarMobile.module.css';
 import Image from 'next/image';
 
 interface NavbarMobileProps {
